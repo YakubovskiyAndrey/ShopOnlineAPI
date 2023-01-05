54d0360fe65f339a567d91372a4f020a875de7e0
Gamer <nelo9997@gmai.com>
2023-01-05 22:57:28 +0200

    version 01/05/23

---
+++ src/test/java/ua/yakubovskiy/rest/controller/BrandControllerTest.java
@@ -0,0 +1,93 @@
+package ua.yakubovskiy.rest.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import ua.yakubovskiy.rest.ShopOnlineApiApplication;
+import ua.yakubovskiy.rest.dto.RestResponse;
+import ua.yakubovskiy.rest.entity.Brand;
+import ua.yakubovskiy.rest.repository.BrandRepository;
+import java.io.UnsupportedEncodingException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = ShopOnlineApiApplication.class)
+@AutoConfigureMockMvc
+class BrandControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private BrandRepository repository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testCreateBrand() throws Exception {
+        String name = "ZTE";
+        String body = """
+          {
+              "name": "%s"
+          }
+        """.formatted(name);
+
+        MvcResult mvcResult = mvc.perform(post("/api/brands")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        RestResponse response = parseResponse(mvcResult);
+        int brandId = Integer.parseInt(response.result());
+        assertThat(brandId).isPositive();
+
+        Brand brand = repository.findById(brandId).orElse(null);
+        assertThat(brand).isNotNull();
+        assertThat(brand.getName().trim()).isEqualTo(name);
+        repository.deleteById(brandId);
+    }
+
+    @Test
+    void testGetBrandById() throws Exception {
+        Brand brand = new Brand();
+        brand.setName("Nokia");
+        repository.save(brand);
+        int brandId = brand.getId();
+
+        mvc.perform(get("/api/brands/" + brandId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Nokia"));
+
+     repository.deleteById(brandId);
+    }
+
+    private RestResponse parseResponse(MvcResult mvcResult) {
+        try {
+            return objectMapper.readValue(mvcResult.getResponse().getContentAsString(), RestResponse.class);
+        } catch (JsonProcessingException | UnsupportedEncodingException e) {
+            throw new RuntimeException("Error parsing json", e);
+        }
+    }
+}
